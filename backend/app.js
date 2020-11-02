9812d1b4959e3a5a1bb1b198c0f576a8d2944569
sidsalvi20 <51946953+sidsalvi20@users.noreply.github.com>
2020-11-03 02:42:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const userRouter = require("./routes/user.js");
 const hospitalRouter = require("./routes/hospital.js");
+const doctorRouter = require("./routes/doctor.js");
 const cors = require('cors');
 app.use(cors());
 
@@ -10,6 +11,7 @@ app.use(cors());
 
 app.use("/api/users",express.json(),userRouter);
 app.use("/api/hospital",express.json(),hospitalRouter);
+app.use("/api/doctor",express.json(),doctorRouter);
 
 
 app.listen(process.env.APP_PORT,() =>{
